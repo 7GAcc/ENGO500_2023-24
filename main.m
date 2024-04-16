cc62241000ccc041045ea0b1889a71d276826786
Zian <zian.zahid@gmail.com>
2024-04-15 22:44:40 -0600

    Changed manual file selection to uigetfile

--- main.m
+++ main.m
@@ -34,30 +34,32 @@ testdata.classes=["Static";
     "Situps"
     ];
 %Load data being tested
-testdata.list=[
+% testdata.list=[
 % "Processed Data/Test Data/PROCESSED_S22_Gaurav_BenchPress_11Reps-undefined-undefined-2024-03-26_20-46-30.mat"
 % "Processed Data/Test Data/PROCESSED_S22_Muaz_BenchPress_10Reps_V-undefined-undefined-2024-03-21_20-06-26.mat"
 % "Processed Data/Test Data/PROCESSED_S22_Muaz_PullupsUnA_9Reps_V-undefined-undefined-2024-03-21_19-55-13.mat"
 % "Processed Data/Test Data/PROCESSED_S22_Muaz_Pullups_15Reps_V-undefined-undefined-2024-03-21_19-59-43.mat"
 % "Processed Data/Test Data/PROCESSED_S22_Muaz_Situps_10Reps_V-undefined-undefined-2024-03-21_20-10-57.mat"
 % "Processed Data/Test Data/PROCESSED_S22_Muaz_Walking_15Steps-undefined-undefined-2024-03-21_20-15-54.mat"
-"Processed Data/Test Data/PROCESSED_S22_Raya_BarbellSquats_OO_10Reps-undefined-undefined-2024-03-19_17-41-14.mat"
-"Processed Data/Test Data/PROCESSED_S22_Raya_BenchPress_OO_10Reps_V-undefined-undefined-2024-03-19_17-13-05.mat"
-"Processed Data/Test Data/PROCESSED_S22_Raya_Jogging_OO_300Steps-undefined-undefined-2024-03-19_16-49-43.mat"
-"Processed Data/Test Data/PROCESSED_S22_Raya_Pullups_OO_7Reps-undefined-undefined-2024-03-19_17-32-35.mat"
-"Processed Data/Test Data/PROCESSED_S22_Raya_Walking_OO_300Steps-undefined-undefined-2024-03-19_16-39-27.mat"
-"Processed Data/Test Data/PROCESSED_S22_Selena_BarbellSquats_10Reps-undefined-undefined-2024-03-27_8-47-49.mat"
-"Processed Data/Test Data/PROCESSED_S22_Selena_BenchPress_OO_8Reps-undefined-undefined-2024-03-20_11-25-08.mat"
-"Processed Data/Test Data/PROCESSED_S22_Selena_Jogging_OO_200Steps-undefined-undefined-2024-03-20_10-43-03.mat"
-"Processed Data/Test Data/PROCESSED_S22_Selena_Pullups_10Reps-undefined-undefined-2024-03-27_9-06-33.mat"
-"Processed Data/Test Data/PROCESSED_S22_Zian_BarbellSquat_OO_10Reps_V-undefined-undefined-2024-03-20_12-01-30.mat"
-"Processed Data/Test Data/PROCESSED_S22_Zian_BarbellSquats_8Reps_plate25-undefined-undefined-2024-03-27_8-35-31.mat"
-"Processed Data/Test Data/PROCESSED_S22_Zian_BenchPress_6Reps-undefined-undefined-2024-03-25_21-11-48.mat"
-"Processed Data/Test Data/PROCESSED_S22_Zian_Deadlift_OO_10Reps-undefined-undefined-2024-03-20_11-42-53.mat"
-"Processed Data/Test Data/PROCESSED_S22_Zian_Jogging_OO_40Steps-undefined-undefined-2024-03-20_10-48-02.mat"
-"Processed Data/Test Data/PROCESSED_S22_Zian_Pullups_15Reps_V-undefined-undefined-2024-03-21_20-02-05.mat"
-"Processed Data/Test Data/PROCESSED_S22_Zian_Situps_OO_12Reps_V-undefined-undefined-2024-03-20_11-12-47.mat"
-    ];
+% "Processed Data/Test Data/PROCESSED_S22_Raya_BarbellSquats_OO_10Reps-undefined-undefined-2024-03-19_17-41-14.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Raya_BenchPress_OO_10Reps_V-undefined-undefined-2024-03-19_17-13-05.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Raya_Jogging_OO_300Steps-undefined-undefined-2024-03-19_16-49-43.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Raya_Pullups_OO_7Reps-undefined-undefined-2024-03-19_17-32-35.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Raya_Walking_OO_300Steps-undefined-undefined-2024-03-19_16-39-27.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Selena_BarbellSquats_10Reps-undefined-undefined-2024-03-27_8-47-49.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Selena_BenchPress_OO_8Reps-undefined-undefined-2024-03-20_11-25-08.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Selena_Jogging_OO_200Steps-undefined-undefined-2024-03-20_10-43-03.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Selena_Pullups_10Reps-undefined-undefined-2024-03-27_9-06-33.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Zian_BarbellSquat_OO_10Reps_V-undefined-undefined-2024-03-20_12-01-30.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Zian_BarbellSquats_8Reps_plate25-undefined-undefined-2024-03-27_8-35-31.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Zian_BenchPress_6Reps-undefined-undefined-2024-03-25_21-11-48.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Zian_Deadlift_OO_10Reps-undefined-undefined-2024-03-20_11-42-53.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Zian_Jogging_OO_40Steps-undefined-undefined-2024-03-20_10-48-02.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Zian_Pullups_15Reps_V-undefined-undefined-2024-03-21_20-02-05.mat"
+% "Processed Data/Test Data/PROCESSED_S22_Zian_Situps_OO_12Reps_V-undefined-undefined-2024-03-20_11-12-47.mat"
+%     ];
+[files,filespath]=uigetfile('*.mat','MultiSelect','on');
+testdata.list=string(filespath)+string(char(files));
 testdata_orig=testdata;
 
 
